0931de49a90a76451aa0dbeb6c76f5493e5d7258
antiquark <top.squark@gmail.com>
2015-01-04 20:30:49 +0200

    Corrected AssemblyInfo

--- projects/Epicycle.Math_cs-Test/Properties/AssemblyInfo.cs
+++ projects/Epicycle.Math_cs-Test/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Metaqube.Math_cs-Test")]
+[assembly: AssemblyTitle("Epicycle.Math_cs-Test")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Metaqube.Math_cs-Test")]
+[assembly: AssemblyProduct("Epicycle.Math_cs-Test")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- projects/Epicycle.Math_cs/Properties/AssemblyInfo.cs
+++ projects/Epicycle.Math_cs/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Metaqube.Math_cs")]
+[assembly: AssemblyTitle("Epicycle.Math_cs")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Metaqube.Math_cs")]
+[assembly: AssemblyProduct("Epicycle.Math_cs")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
