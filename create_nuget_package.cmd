73623558b4620c4be6dbadb0bfd5d0a7374f5ca7
Dima Potekhin <untrots@gmail.com>
2015-01-08 16:33:31 +0200

    version: 0.1.1.0 => 0.1.2.0

--- create_nuget_package.cmd
+++ create_nuget_package.cmd
@@ -2,34 +2,34 @@
 
 rmdir NuGetPackage /s /q
 mkdir NuGetPackage
-mkdir NuGetPackage\Epicycle.Math-cs.0.1.1.0
-mkdir NuGetPackage\Epicycle.Math-cs.0.1.1.0\lib
+mkdir NuGetPackage\Epicycle.Math-cs.0.1.2.0
+mkdir NuGetPackage\Epicycle.Math-cs.0.1.2.0\lib
 
-copy package.nuspec NuGetPackage\Epicycle.Math-cs.0.1.1.0\Epicycle.Math-cs.0.1.1.0.nuspec
-copy README.md NuGetPackage\Epicycle.Math-cs.0.1.1.0\README.md
-copy LICENSE NuGetPackage\Epicycle.Math-cs.0.1.1.0\LICENSE
+copy package.nuspec NuGetPackage\Epicycle.Math-cs.0.1.2.0\Epicycle.Math-cs.0.1.2.0.nuspec
+copy README.md NuGetPackage\Epicycle.Math-cs.0.1.2.0\README.md
+copy LICENSE NuGetPackage\Epicycle.Math-cs.0.1.2.0\LICENSE
 
-xcopy externals\lib_dotnet\Clipper.6.2.1\lib\clipper_library.dll NuGetPackage\Epicycle.Math-cs.0.1.1.0\lib\net40\
-xcopy externals\lib_dotnet\Clipper.6.2.1\lib\clipper_library.pdb NuGetPackage\Epicycle.Math-cs.0.1.1.0\lib\net40\
-xcopy bin\net40\Release\Epicycle.Math.TestUtils_cs.dll NuGetPackage\Epicycle.Math-cs.0.1.1.0\lib\net40\
-xcopy bin\net40\Release\Epicycle.Math.TestUtils_cs.pdb NuGetPackage\Epicycle.Math-cs.0.1.1.0\lib\net40\
-xcopy bin\net40\Release\Epicycle.Math.TestUtils_cs.xml NuGetPackage\Epicycle.Math-cs.0.1.1.0\lib\net40\
-xcopy bin\net40\Release\Epicycle.Math.Windows_cs.dll NuGetPackage\Epicycle.Math-cs.0.1.1.0\lib\net40\
-xcopy bin\net40\Release\Epicycle.Math.Windows_cs.pdb NuGetPackage\Epicycle.Math-cs.0.1.1.0\lib\net40\
-xcopy bin\net40\Release\Epicycle.Math.Windows_cs.xml NuGetPackage\Epicycle.Math-cs.0.1.1.0\lib\net40\
-xcopy bin\net40\Release\Epicycle.Math_cs.dll NuGetPackage\Epicycle.Math-cs.0.1.1.0\lib\net40\
-xcopy bin\net40\Release\Epicycle.Math_cs.pdb NuGetPackage\Epicycle.Math-cs.0.1.1.0\lib\net40\
-xcopy bin\net40\Release\Epicycle.Math_cs.xml NuGetPackage\Epicycle.Math-cs.0.1.1.0\lib\net40\
-xcopy externals\lib_dotnet\Clipper.6.2.1\lib\clipper_library.dll NuGetPackage\Epicycle.Math-cs.0.1.1.0\lib\net45\
-xcopy externals\lib_dotnet\Clipper.6.2.1\lib\clipper_library.pdb NuGetPackage\Epicycle.Math-cs.0.1.1.0\lib\net45\
-xcopy bin\net45\Release\Epicycle.Math.TestUtils_cs.dll NuGetPackage\Epicycle.Math-cs.0.1.1.0\lib\net45\
-xcopy bin\net45\Release\Epicycle.Math.TestUtils_cs.pdb NuGetPackage\Epicycle.Math-cs.0.1.1.0\lib\net45\
-xcopy bin\net45\Release\Epicycle.Math.TestUtils_cs.xml NuGetPackage\Epicycle.Math-cs.0.1.1.0\lib\net45\
-xcopy bin\net45\Release\Epicycle.Math.Windows_cs.dll NuGetPackage\Epicycle.Math-cs.0.1.1.0\lib\net45\
-xcopy bin\net45\Release\Epicycle.Math.Windows_cs.pdb NuGetPackage\Epicycle.Math-cs.0.1.1.0\lib\net45\
-xcopy bin\net45\Release\Epicycle.Math.Windows_cs.xml NuGetPackage\Epicycle.Math-cs.0.1.1.0\lib\net45\
-xcopy bin\net45\Release\Epicycle.Math_cs.dll NuGetPackage\Epicycle.Math-cs.0.1.1.0\lib\net45\
-xcopy bin\net45\Release\Epicycle.Math_cs.pdb NuGetPackage\Epicycle.Math-cs.0.1.1.0\lib\net45\
-xcopy bin\net45\Release\Epicycle.Math_cs.xml NuGetPackage\Epicycle.Math-cs.0.1.1.0\lib\net45\
+xcopy externals\lib_dotnet\Clipper.6.2.1\lib\clipper_library.dll NuGetPackage\Epicycle.Math-cs.0.1.2.0\lib\net40\
+xcopy externals\lib_dotnet\Clipper.6.2.1\lib\clipper_library.pdb NuGetPackage\Epicycle.Math-cs.0.1.2.0\lib\net40\
+xcopy bin\net40\Release\Epicycle.Math.TestUtils_cs.dll NuGetPackage\Epicycle.Math-cs.0.1.2.0\lib\net40\
+xcopy bin\net40\Release\Epicycle.Math.TestUtils_cs.pdb NuGetPackage\Epicycle.Math-cs.0.1.2.0\lib\net40\
+xcopy bin\net40\Release\Epicycle.Math.TestUtils_cs.xml NuGetPackage\Epicycle.Math-cs.0.1.2.0\lib\net40\
+xcopy bin\net40\Release\Epicycle.Math.Windows_cs.dll NuGetPackage\Epicycle.Math-cs.0.1.2.0\lib\net40\
+xcopy bin\net40\Release\Epicycle.Math.Windows_cs.pdb NuGetPackage\Epicycle.Math-cs.0.1.2.0\lib\net40\
+xcopy bin\net40\Release\Epicycle.Math.Windows_cs.xml NuGetPackage\Epicycle.Math-cs.0.1.2.0\lib\net40\
+xcopy bin\net40\Release\Epicycle.Math_cs.dll NuGetPackage\Epicycle.Math-cs.0.1.2.0\lib\net40\
+xcopy bin\net40\Release\Epicycle.Math_cs.pdb NuGetPackage\Epicycle.Math-cs.0.1.2.0\lib\net40\
+xcopy bin\net40\Release\Epicycle.Math_cs.xml NuGetPackage\Epicycle.Math-cs.0.1.2.0\lib\net40\
+xcopy externals\lib_dotnet\Clipper.6.2.1\lib\clipper_library.dll NuGetPackage\Epicycle.Math-cs.0.1.2.0\lib\net45\
+xcopy externals\lib_dotnet\Clipper.6.2.1\lib\clipper_library.pdb NuGetPackage\Epicycle.Math-cs.0.1.2.0\lib\net45\
+xcopy bin\net45\Release\Epicycle.Math.TestUtils_cs.dll NuGetPackage\Epicycle.Math-cs.0.1.2.0\lib\net45\
+xcopy bin\net45\Release\Epicycle.Math.TestUtils_cs.pdb NuGetPackage\Epicycle.Math-cs.0.1.2.0\lib\net45\
+xcopy bin\net45\Release\Epicycle.Math.TestUtils_cs.xml NuGetPackage\Epicycle.Math-cs.0.1.2.0\lib\net45\
+xcopy bin\net45\Release\Epicycle.Math.Windows_cs.dll NuGetPackage\Epicycle.Math-cs.0.1.2.0\lib\net45\
+xcopy bin\net45\Release\Epicycle.Math.Windows_cs.pdb NuGetPackage\Epicycle.Math-cs.0.1.2.0\lib\net45\
+xcopy bin\net45\Release\Epicycle.Math.Windows_cs.xml NuGetPackage\Epicycle.Math-cs.0.1.2.0\lib\net45\
+xcopy bin\net45\Release\Epicycle.Math_cs.dll NuGetPackage\Epicycle.Math-cs.0.1.2.0\lib\net45\
+xcopy bin\net45\Release\Epicycle.Math_cs.pdb NuGetPackage\Epicycle.Math-cs.0.1.2.0\lib\net45\
+xcopy bin\net45\Release\Epicycle.Math_cs.xml NuGetPackage\Epicycle.Math-cs.0.1.2.0\lib\net45\
 
 pause
