58d8da289e633e4f08e07e80f300e08d1853885b
Dima Potekhin <untrots@gmail.com>
2015-01-08 15:50:37 +0200

    Adding net40 framework (does not compile yet)

--- create_nuget_package.cmd
+++ create_nuget_package.cmd
@@ -9,6 +9,17 @@ copy package.nuspec NuGetPackage\Epicycle.Math-cs.0.1.1.0\Epicycle.Math-cs.0.1.1
 copy README.md NuGetPackage\Epicycle.Math-cs.0.1.1.0\README.md
 copy LICENSE NuGetPackage\Epicycle.Math-cs.0.1.1.0\LICENSE
 
+xcopy externals\lib_dotnet\Clipper.6.2.1\lib\clipper_library.dll NuGetPackage\Epicycle.Math-cs.0.1.1.0\lib\net40\
+xcopy externals\lib_dotnet\Clipper.6.2.1\lib\clipper_library.pdb NuGetPackage\Epicycle.Math-cs.0.1.1.0\lib\net40\
+xcopy bin\net40\Release\Epicycle.Math.TestUtils_cs.dll NuGetPackage\Epicycle.Math-cs.0.1.1.0\lib\net40\
+xcopy bin\net40\Release\Epicycle.Math.TestUtils_cs.pdb NuGetPackage\Epicycle.Math-cs.0.1.1.0\lib\net40\
+xcopy bin\net40\Release\Epicycle.Math.TestUtils_cs.xml NuGetPackage\Epicycle.Math-cs.0.1.1.0\lib\net40\
+xcopy bin\net40\Release\Epicycle.Math.Windows_cs.dll NuGetPackage\Epicycle.Math-cs.0.1.1.0\lib\net40\
+xcopy bin\net40\Release\Epicycle.Math.Windows_cs.pdb NuGetPackage\Epicycle.Math-cs.0.1.1.0\lib\net40\
+xcopy bin\net40\Release\Epicycle.Math.Windows_cs.xml NuGetPackage\Epicycle.Math-cs.0.1.1.0\lib\net40\
+xcopy bin\net40\Release\Epicycle.Math_cs.dll NuGetPackage\Epicycle.Math-cs.0.1.1.0\lib\net40\
+xcopy bin\net40\Release\Epicycle.Math_cs.pdb NuGetPackage\Epicycle.Math-cs.0.1.1.0\lib\net40\
+xcopy bin\net40\Release\Epicycle.Math_cs.xml NuGetPackage\Epicycle.Math-cs.0.1.1.0\lib\net40\
 xcopy externals\lib_dotnet\Clipper.6.2.1\lib\clipper_library.dll NuGetPackage\Epicycle.Math-cs.0.1.1.0\lib\net45\
 xcopy externals\lib_dotnet\Clipper.6.2.1\lib\clipper_library.pdb NuGetPackage\Epicycle.Math-cs.0.1.1.0\lib\net45\
 xcopy bin\net45\Release\Epicycle.Math.TestUtils_cs.dll NuGetPackage\Epicycle.Math-cs.0.1.1.0\lib\net45\
